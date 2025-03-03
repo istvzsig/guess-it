291bc5b07656a6543e8e7509906039885a8cceff
istvzsig <istvanzsigmond@aol.com>
2025-03-03 12:01:19 +0100

    update env

--- src/app/environments/api.environment.ts
+++ src/app/environments/api.environment.ts
@@ -1,1 +1,2 @@
-export const OPEN_TDB_API_URL = 'https://opentdb.com/api.php?amount=S';
+export const OPEN_TDB_API_URL = 'https://opentdb.com/api.php?amount=1';
+export const DUMMY_CACHE_KEY = 'DUMMY_CACHE_KEY';
